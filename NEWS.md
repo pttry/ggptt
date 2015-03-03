b8f368abe2abf1db977bbf6b21caeac46ca209b7
Janne Huovari <janne.huovari@ptt.fi>
2015-03-03 15:32:44 +0200

    the_legeng_bot

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,7 @@
 ggptt 0.2.0
 ===========
-* `the_x45()` tab x-axis text in 45 angle.
+* `the_x45()` for x-axis text in 45 angle.
+* `the_legend_bot for legend.position = "bottom"`
 * `ptt_pal()` color PTT palette. 6 colours.
 * `scale_colour_ptt()` and `scale_fill_ptt()` for discrete PTT colour scales
 
--- R/theme_incompletes.R
+++ R/theme_incompletes.R
@@ -9,6 +9,11 @@
 #'
 #' \tabular{ll}{
 #'    the_x45 \tab x-axis text in 45 angle \cr
+#'    the_legend_bot \tab legend.position = "bottom" \cr
 #'  }
 #'
 the_x45 <- function() theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#' @describeIn the_x45
+#' @export
+the_legend_bot <- function() theme(legend.position = "bottom")
