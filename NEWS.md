cfb5ce57b515ae39b420dfe1a3a88d592da9b0d9
Janne Huovari <janne.huovari@ptt.fi>
2018-07-25 15:06:18 +0300

    halfq_shift

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,13 @@
+ggplot 1.1.0
+============
+
+* `halfq_shift()` to shift time a half quarter in quarter charts.
+
+ggplot 1.0.0
+============
+
+* To github
+
 ggptt 0.4.3
 
 * PTT default colour to geom_col
---
+++ R/time_shift.R
@@ -0,0 +1,11 @@
+#' Shift time 1.5 months
+#'
+#' Used to shift time a half quarter in quarter charts
+#'
+#' @param x a Date vector to shift
+#'
+#' @export
+#'
+halfq_shift <- function(x){
+  x + months(1) + days(14)
+}
