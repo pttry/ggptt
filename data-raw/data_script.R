5d21b1e58f9a49befe857e1929c27f5746516c67
Janne Huovari <janne.huovari@ptt.fi>
2019-07-31 14:31:49 +0300

    more colours and in hex

--- data-raw/data_script.R
+++ data-raw/data_script.R
@@ -4,8 +4,9 @@
 ggptt_palettes <- list(
   ptt_new = c("#00713D", "#00627D", "#C36B0D", "#330099", "#FF6600",
           "#00A756", "#1FBADB", "#F3CF95", "#6666CC", "#FFCC00"),
-  ptt = c("darkolivegreen4", "cornflowerblue", "darkorange2",
-          "coral4", "mediumpurple3", "grey50", "#6666CC"),
+  ptt = c("#6E8B3D", "#6495ED", "#EE7600", "#8B3E2F", "#8968CD",
+          "#7F7F7F", "#6666CC","#00713D", "#00627D", "#C36B0D", "#330099",
+          "#FF6600", "#00A756", "#1FBADB", "#F3CF95","#FFCC00", "#FF0000"),
   vnk = c(rgb(0, 111, 185, maxColorValue = 255),
           rgb(10, 187, 236, maxColorValue = 255),
           rgb(122, 136, 144, maxColorValue = 255),
