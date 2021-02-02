1069fd448e2201e61dabe2f9a419bfe61ffe779c
Juho Alasalmi <juho.alasalmi@ptt.fi>
2021-02-02 15:03:41 +0100

    improve ptt_draw_map

--- R/ptt_draw_map.R
+++ R/ptt_draw_map.R
@@ -2,14 +2,14 @@
 #'
 #' Tämä funktio piirtää datan Suomen kartalle käyttäjän määrittelemällä aluejaolla ja aluejaon vuodella.
 #'
-# Huom. ottaa tällä hetkellä datan vastaan ainoastaan pitkässä muodossa. Argumentti x on arvo muuttujassa
-# 'tiedot_code'
 #'
 #' @param data data.frame
 #' @param vuosi kartan vuosi
 #' @param aluejako kartan aluejako
-#' @param x muuttuja
+#' @param x arvo muuttujassa \code{tiedot_code}
 #' @param time
+#' @param grid logical, whether grid with coordinates is drawn. Defaults to TRUE
+#' @param long_data logical, whether input data is in the long form. Defaults to TRUE.
 #'
 #' @return figure
 #' @export dplyr
@@ -17,11 +17,28 @@
 #'
 #' @examples
 #'
-#' pttdatahaku::ptt_read_data("tyonv_1001", "kunta") %>% ptt_draw_map(2020, "kunta", "TYOTOSUUS")
-#' pttdatahaku::ptt_read_data("tyonv_1001", "seutukunta") %>% ptt_draw_map(2020, "seutukunta", "TYOTOSUUS")
-#' pttdatahaku::ptt_read_data("tyonv_1001", "maakunta") %>% ptt_draw_map(2020, "maakunta", "TYOTOSUUS")
+#' pttdatahaku::ptt_read_data("tyonv_1001", "kunta") %>%
+#'      ptt_draw_map(2020, "kunta", "TYOTOSUUS")
+#' pttdatahaku::ptt_read_data("tyonv_1001", "seutukunta") %>%
+#'      ptt_draw_map(2020, "seutukunta", "TYOTOSUUS")
+#' pttdatahaku::ptt_read_data("tyonv_1001", "maakunta") %>%
+#'      ptt_draw_map(2020, "maakunta", "TYOTOSUUS") +
+#'      scale_fill_gradient(low = "white", high = ggptt_palettes$ptt_new[1]) +
+#'        theme(legend.position = "top",
+#'              legend.justification = "left")
 #'
-ptt_draw_map <- function(data, vuosi = substring(max(data$time), 1,4), aluejako, x, time = max(data$time)) {
+ptt_draw_map <- function(data,
+                         vuosi = substring(max(data$time), 1,4),
+                         aluejako, x,
+                         time = max(data$time),
+                         grid = TRUE,
+                         long_data = TRUE) {
+
+  if(!is.null(attributes(data)$codes_names$tiedot)) {
+    codes_names_tiedot <-attributes(data)$codes_names$tiedot
+  } else {
+    codes_names_tiedot <- NULL
+  }
 
   # Get a list of all available maps
   capabilities <- xml2::read_xml("https://geo.stat.fi/geoserver/tilastointialueet/wfs?service=WFS&version=2.0.0&request=GetCapabilities")
@@ -48,16 +65,32 @@ ptt_draw_map <- function(data, vuosi = substring(max(data$time), 1,4), aluejako,
   prefix_name_key <- c("kunta" = "KU", "seutukunta" = "SK", "maakunta" = "MK", "suuralue" = "SA")
 
   # Get map and modify region variable
-  map <- sf::st_read(httr::build_url(url))
+  map <- sf::st_read(httr::build_url(url), quiet = TRUE)
   map[[aluejako]] <- paste0(prefix_name_key[aluejako], map[[aluejako]])
   map <- dplyr::rename_with(map, ~paste0(aluejako, "_code"), aluejako)
 
   # Filter required from the input data
+
+  if(long_data) {
   data <- dplyr::filter(data, tiedot_code == x, time == time) %>%
           tidyr::spread(tiedot_code, values)
+  }
 
   output <- map %>%
              dplyr::left_join(data, by = paste0(aluejako, "_code")) %>%
-             ggplot(aes_string(fill = x)) + geom_sf()
+             ggplot(aes_string(fill = x)) +
+                 geom_sf() +
+                 labs(fill = ifelse(!is.null(codes_names_tiedot),
+                                    codes_names_tiedot[x], x))
+
+  if(!grid) {
+    output <- output + theme(panel.grid.major = element_line(colour = "transparent"),
+                             panel.border = element_rect(color = "transparent"),
+                             plot.background = element_rect(color = "transparent")) +
+                       coord_sf(datum = NA)
+  }
+
+
   output
 }
+
