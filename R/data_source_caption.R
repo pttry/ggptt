f5e2d641a0eb0390688b308ad9db200876e8b6c0
Juho Alasalmi <juho.alasalmi@ptt.fi>
2021-02-02 15:12:39 +0100

    Add example to data_source_caption

--- R/data_source_caption.R
+++ R/data_source_caption.R
@@ -8,6 +8,16 @@
 #' @export
 #'
 #' @examples
+#'
+#' ptt_read_data("tyonv_1001", "KOKO MAA") %>%
+#'   filter(tiedot_code == "AVPAIKAT") %>%
+#'   { ggplot(., aes(x = time, y = values)) +
+#'     geom_line() +
+#'     geom_smooth() +
+#'     labs(x = NULL,
+#'          y = attributes(.)$codes_names$tiedot["AVPAIKAT"]) +
+#'      data_source_caption(.)
+#'    }
 data_source_caption <- function(data) {
 
   if(is.null(attributes(data)$citation)) {
