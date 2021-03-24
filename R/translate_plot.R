91bc517e280652e8cb8e8fdc1b789b3b4d795b7d
Janne Huovari <janne.huovari@ptt.fi>
2021-03-24 11:39:17 +0200

    translation

--- R/translate_plot.R
+++ R/translate_plot.R
@@ -11,17 +11,61 @@
 #' translation <- c("Test plot" = "Testikuvio", wt = "Paino", mpg = "Mailia per gallona")
 #' p2 <- translate_plot(p, translation)
 #'
+#' library(patchwork)
+#' pp <- p / ((p + geom_point(colour = "blue")) + (p + geom_point(colour = "red")))
+#' pp2 <- translate_plot(pp, translation)
 translate_plot <- function(plot, trans){
 
-  mod_data <- plot$data %>%
-    mutate(across(where(is.factor), ~recode(.x, !!!trans))) %>%
-    mutate(across(where(is.character), ~recode(.x, !!!trans)))
 
-  plot$data <- mod_data
+  if (inherits(plot, "patchwork")){
+
+    patches <- patchwork:::get_patches(plot)
+    mod_plots[[1]] <- translator(patches$plots[[1]], trans)
+    mod_plots[[2]][[1]] <- translator(patches$plots[[2]][[1]], trans)
+    mod_plots[[2]][[2]] <- translator(patches$plots[[2]][[2]], trans)
+    # plots <- plot$patches$plots
+    # mod_plots <- purrr::map(patches$plots, ~translator(.x, trans))
+    # mod_plots[[2]][[3]] <- NULL
+    # plot$patches$plots <- mod_patches
+
+    plot$patches$plots <- mod_plots
+
+    # plot <- patchwork:::add_patches(plot, patches)
+    return(plot)
+  } else {
+    plot <- translator(plot, trans)
+    return(plot)
+  }
+
+}
+
+translator <- function(plot, trans){
+  # print("ok")
+  # print(class(plot))
+  #
+  # if (inherits(plot, "patchwork")){
+  #   # purrr::map(plot, ~print("ku"))
+  #   patches <- patchwork:::get_patches(plot)
+  #   print(length(patches))
+  #   mod_plot <- purrr::map(plot, ~translator(.x, trans))
+  #   return(mod_plot)
+  # }
+
+  if (inherits(plot, "gg")){
+    mod_data <- plot$data %>%
+      mutate(across(where(is.factor), ~recode(.x, !!!trans))) %>%
+      mutate(across(where(is.character), ~recode(.x, !!!trans)))
+
+    plot$data <- mod_data
 
 
-  plot$labels <- plot$labels %>%
-    purrr::map( ~recode(.x, !!!trans))
+    plot$labels <- plot$labels %>%
+      purrr::map( ~recode(.x, !!!trans))
+
+  } else {
+   plot <- plot
+  }
+
 
   plot
 
