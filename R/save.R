7fbc57a2817ab270b209320eaed3e9d0ef89f821
Janne Huovari <janne.huovari@ptt.fi>
2021-05-07 10:08:51 +0300

    ggsave_blog

--- R/save.R
+++ R/save.R
@@ -22,3 +22,29 @@ ggsave_twitter <- function(filename,
                   width = 16/2,
                   height = 9/2)
 }
+
+
+#'Save plots for twitter
+#'
+#' @param filename File name to create on disk without extension
+#' @param file_path A path to save file. Default is user Downloads folder.
+#' @param plot Plot to save, defaults to last plot displayed.
+#'
+#'
+#' @export
+#'
+ggsave_blog <- function(filename,
+                           file_path = file.path(Sys.getenv("USERPROFILE"),"Downloads"),
+                           plot = last_plot()){
+  plot2 = plot +
+    ggplot2::theme(text = element_text(size = 14)) +
+    ggplot2::theme(
+      plot.subtitle = ggplot2::element_text(colour = "grey40"),
+      plot.caption = ggplot2::element_text(size = 10, face = "plain", colour = "grey40"),
+      text = ggplot2::element_text(face = "plain"),
+      plot.margin = ggplot2::margin(1, 1, 2, 1))
+  ggplot2::ggsave(file.path(file_path, paste0(filename, ".png")),
+                  plot = plot2,
+                  width = 7,
+                  height = 5)
+}
