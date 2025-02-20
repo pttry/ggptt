bdb394f2d9ef826262013875eff8d898e02558c5
jalasalmi <juho.alasalmi@ptt.fi>
2025-02-20 10:15:43 +0200

    get_alt_text() first commit

---
+++ R/alt_text.R
@@ -0,0 +1,23 @@
+#' Generate alt-text
+#'
+#' Generates alt-text for the last drawn plot.
+#'
+#' Currently uses Google's Gemini. You have to provide `GOOGLE_API_KEY` in the
+#' `.Renviron`. For more, see R-package `ellmer`.
+#'
+#' @return alt-text
+#' @export
+#'
+#' @examples
+gen_alt_text <- function() {
+
+  chat <- ellmer::chat_gemini(system_prompt = "You are a statistician, precise and brief. You write in Finnish.")
+
+  chat$chat(
+    "Describe this plot in one paragraph, as suitable for inclusion in
+   alt-text. You should briefly describe the plot type, the axes, and
+   2-5 major visual patterns.",
+    content_image_plot()
+  )
+
+}
